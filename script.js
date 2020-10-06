966cbf372a868edbb9f7a90488d88679329ef119
Clockwork25 <67840273+Clockwork25@users.noreply.github.com>
2020-10-06 09:07:34 -0700

    Update 4

--- script.js
+++ script.js
@@ -2,13 +2,9 @@ function mouseAnim() {
     var elem = document.getElementById("mouse-wheel");   
     var pos = 0;
     var id = setInterval(step, 50);
-    function step() {
+
+function step() {
         pos++; 
         elem.style.backgroundPositionY = pos + "px"; 
       }
-}
-
-window.onscroll = function() {myFunction()};
-function myfunction(){
-  return;
-}
+    }
