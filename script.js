bb5771762213c78b7d3eba7943f506c239d1cc92
Clockwork25 <67840273+Clockwork25@users.noreply.github.com>
2020-10-06 06:47:09 -0700

    Removed sticky navBar

--- script.js
+++ script.js
@@ -8,17 +8,7 @@ function mouseAnim() {
       }
 }
 
-
-
 window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+function myfunction(){
+  return;
 }
