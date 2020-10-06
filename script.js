5929bb5b2fd8d62db21ae19fe1a1b2f8b1b00522
Clockwork25 <67840273+Clockwork25@users.noreply.github.com>
2020-10-06 11:52:04 -0700

    Update 6
    Fade in logos in skills section

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ $(document).ready(function(){
   $( 'a:hidden' ).fadeIn( 2000 ).removeClass('hidden-title')
   $( 'div:hidden' ).fadeIn( 2000 ).removeClass('hidden-name')
 });
+
+$(document).ready(function() {
+    
+  /* Every time the window is scrolled ... */
+  $(window).scroll( function(){
+  
+      /* Check the location of each desired element */
+      $('.skills-logo').each( function(i){
+          
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight()+20;
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          
+          /* If the object is completely visible in the window, fade it it */
+          if( bottom_of_window > bottom_of_object ){
+              
+              $(this).animate({'opacity':'1'},1000);
+                  
+          }
+          
+      }); 
+  
+  });
+  
+});
